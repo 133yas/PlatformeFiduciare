eec293e18dfdd15889514f155a44dc492e8c1a88
133yas <80778794+133yas@users.noreply.github.com>
2024-03-08 17:02:58 +0100

    EtatDemande and TypeDemande

--- src/main/java/org/example/fudiciare/bean/Demande/EtatDemande.java
+++ src/main/java/org/example/fudiciare/bean/Demande/EtatDemande.java
@@ -1,4 +1,40 @@
 package org.example.fudiciare.bean.Demande;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class EtatDemande {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String libelle;
+    private String code;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
+
--- src/main/java/org/example/fudiciare/bean/Demande/TypeDemande.java
+++ src/main/java/org/example/fudiciare/bean/Demande/TypeDemande.java
@@ -1,4 +1,58 @@
 package org.example.fudiciare.bean.Demande;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class TypeDemande {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String Libelle;
+
+    private String code;
+    private double honnoraireComptableTraitant;
+    private double honnoraireComptableValidateur;
+
+    public double getHonnoraireComptableTraitant() {
+        return honnoraireComptableTraitant;
+    }
+
+    public void setHonnoraireComptableTraitant(double honnoraireComptableTraitant) {
+        this.honnoraireComptableTraitant = honnoraireComptableTraitant;
+    }
+
+    public double getHonnoraireComptableValidateur() {
+        return honnoraireComptableValidateur;
+    }
+
+    public void setHonnoraireComptableValidateur(double honnoraireComptableValidateur) {
+        this.honnoraireComptableValidateur = honnoraireComptableValidateur;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getLibelle() {
+        return Libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        Libelle = libelle;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        code = code;
+    }
 }
